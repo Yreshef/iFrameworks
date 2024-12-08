df3cfa823dea86f0fe62e9734184fe1267a7e1e7
Yohai Reshef <yohai.reshef@gmail.com>
2024-12-08 21:19:05 +0200

    Initial Commit

---
+++ iFrameworks/iFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFrameworksApp.swift
+//  iFrameworks
+//
+//  Created by Yohai on 08/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct iFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
